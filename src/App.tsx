31cb487d2d2bab17c2202f89a154e3765b53e92c
eastflag <dklee98@>
2021-04-21 18:58:31 +0900

    board view

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import BoardList from "./pages/board-list/BoardList";
 import {BrowserRouter, Switch, Route} from "react-router-dom";
 import BoardRegister from './pages/board-register/BoardRegister';
 import BoardView from "./pages/board-view/BoardView";
+import BoardEdit from "./pages/board-edit/BoardEdit";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route exact path="/" component={BoardList}></Route>
           <Route path="/board-register" component={BoardRegister}></Route>
           <Route path="/board-view/:id" component={BoardView}></Route>
+          <Route path="/board-edit/:id" component={BoardEdit}></Route>
         </Switch>
       </BrowserRouter>
     </Container>
--- src/pages/board-view/BoardView.tsx
+++ src/pages/board-view/BoardView.tsx
@@ -1,9 +1,9 @@
 import React, {useEffect, useState} from 'react';
-import {Card, Row} from "react-bootstrap";
+import {Button, Card, Col, Row} from "react-bootstrap";
 import axios from "axios";
 import {Board} from "../../dto/Board";
 
-const MyComponent = ({match}: any) => {
+const MyComponent = ({match, history}: any) => {
   const [board, setBoard] = useState<Board>({
     title: '',
     content: ''
@@ -22,12 +22,18 @@ const MyComponent = ({match}: any) => {
 
   return (
     <>
-      <Card className="p-3">
+      <Row className="justify-content-end">
+        <Button variant="info" onClick={() => history.push(`/board-edit/${match.params.id}`)}>수정</Button>
+      </Row>
+      <Card className="p-3 my-3">
         <Card.Title className="pb-2" style={{borderBottom: '1px solid #dddddd'}}>{board?.title}</Card.Title>
         <Card.Text>
           {board?.content}
         </Card.Text>
       </Card>
+      <Row className="justify-content-center">
+        <Button variant="primary" onClick={() => history.goBack()}>돌아가기</Button>
+      </Row>
     </>
   );
 };
