9987fa9773ad4818f49b2c31161353ccd39c2e6a
eastflag <dklee98@>
2021-05-14 17:22:37 +0900

    navigation href 문제점 - 재로딩

--- src/App.tsx
+++ src/App.tsx
@@ -11,33 +11,29 @@ import {Nav, Navbar, NavDropdown} from "react-bootstrap";
 function App() {
   return (
     <Container fluid className="p-0">
-      <BrowserRouter>
-        <Navbar bg="light" expand="lg">
-          <Navbar.Brand href="#home">HOME</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav bd-highlight">
-            <Nav className="mr-auto flex-grow-1">
-              <Nav.Link href="#home">게시판</Nav.Link>
-              <Nav.Link href="#link">등록</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-              </NavDropdown>
-              <span className="flex-grow-1"></span>
-              <Nav.Link href="#link">로그인</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-        <Switch>
-          <Route exact path="/" component={BoardList}></Route>
-          <Route path="/board-register" component={BoardRegister}></Route>
-          <Route path="/board-view/:id" component={BoardView}></Route>
-          <Route path="/board-edit/:id" component={BoardEdit}></Route>
-        </Switch>
-      </BrowserRouter>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">HOME</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto flex-grow-1">
+            <Nav.Link href="/">게시판</Nav.Link>
+            <Nav.Link href="/board-register">등록</Nav.Link>
+            <span className="flex-grow-1"></span>
+            <Nav.Link href="/login">로그인</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+
+      <Container fluid className="px-3 py-2">
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={BoardList}></Route>
+            <Route path="/board-register" component={BoardRegister}></Route>
+            <Route path="/board-view/:id" component={BoardView}></Route>
+            <Route path="/board-edit/:id" component={BoardEdit}></Route>
+          </Switch>
+        </BrowserRouter>
+      </Container>
     </Container>
   );
 }
