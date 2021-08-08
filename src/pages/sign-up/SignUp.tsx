1de1e9f1d77474cbb64563cebdde50b96b211d8d
eastflag <eastflag@gmail.com>
2021-08-09 02:15:04 +0900

    회원가입 폼 변경 - responsive

--- src/pages/sign-up/SignUp.tsx
+++ src/pages/sign-up/SignUp.tsx
@@ -1,6 +1,6 @@
 import { Formik } from 'formik';
 import React from 'react';
-import {Button, Form} from "react-bootstrap";
+import {Button, Col, Form, Row} from "react-bootstrap";
 import * as Yup from "yup";
 import axios from "axios";
 import {toast} from 'react-toastify';
@@ -35,76 +35,85 @@ const SignUp = (props: any) => {
     }
   }
   return (
-    <Formik
-      initialValues={{ email: '', password: '', password2: '', username: ''}}
-      onSubmit={submit}
-      validationSchema={Yup.object().shape({
-        email: Yup.string()
-          .email("이메일형식으로 입력하세요")
-          .required("필수필드 입니다."),
-        password: Yup.string()
-          .required("Required")
-          .min(6, "6자이상 입력하세요"),
-        password2: Yup.string()
-          .oneOf([Yup.ref("password"), null], "패스워드가 일치하지 않습니다.")
-          .required("패스워드가 일치하지 않습니다."),
-        username: Yup.string()
-          .required("필수필드 입니다.")
-      })}>
-      {
-        ({values,
-            errors,
-            touched,
-            handleChange,
-            handleBlur,
-            handleSubmit,
-            isSubmitting }) => (<Form onSubmit={handleSubmit}>
-          <Form.Group controlId="email">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control name="email" placeholder="Enter email"
-                          value={values.email}
-                          onChange={handleChange} onBlur={handleBlur}
-                          isValid={touched.email && !errors.email}
-                          isInvalid={touched.email && errors.email ? true : false} />
-            { touched.email && !errors.email && <Form.Control.Feedback type="valid">Looks good!</Form.Control.Feedback> }
-            { touched.email && errors.email && <Form.Control.Feedback type="invalid">{errors.email}</Form.Control.Feedback> }
-          </Form.Group>
-          <Form.Group controlId="formGroupPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" name="password" placeholder="enter Password"
-                          value={values.password}
-                          onChange={handleChange} onBlur={handleBlur}
-                          isValid={touched.password && !errors.password}
-                          isInvalid={touched.password && errors.password ? true : false} />
-            { touched.password && !errors.password && <Form.Control.Feedback type="valid">Looks good!</Form.Control.Feedback> }
-            { touched.password && errors.password && <Form.Control.Feedback type="invalid">{errors.password}</Form.Control.Feedback> }
-          </Form.Group>
-          <Form.Group controlId="formGroupPassword2">
-            <Form.Label>confirm Password</Form.Label>
-            <Form.Control type="password" name="password2" placeholder="confirm Password"
-                          value={values.password2}
-                          onChange={handleChange} onBlur={handleBlur}
-                          isValid={touched.password2 && !errors.password2}
-                          isInvalid={touched.password2 && errors.password2 ? true : false} />
-            { touched.password2 && !errors.password2 && <Form.Control.Feedback type="valid">Looks good!</Form.Control.Feedback> }
-            { touched.password2 && errors.password2 && <Form.Control.Feedback type="invalid">{errors.password2}</Form.Control.Feedback> }
-          </Form.Group>
-          <Form.Group controlId="formGroupName">
-            <Form.Label>Name</Form.Label>
-            <Form.Control type="text" name="username" placeholder="enter username"
-                          value={values.username}
-                          onChange={handleChange} onBlur={handleBlur}
-                          isValid={touched.username && !errors.username}
-                          isInvalid={touched.username && errors.username ? true : false} />
-            { touched.username && !errors.username && <Form.Control.Feedback type="valid">Looks good!</Form.Control.Feedback> }
-            { touched.username && errors.username && <Form.Control.Feedback type="invalid">{errors.username}</Form.Control.Feedback> }
-          </Form.Group>
-          <Button variant="primary" type="submit" disabled={isSubmitting}>
-            Submit
-          </Button>
-        </Form>)
-      }
-    </Formik>
+    <>
+      <div className="d-flex justify-content-center my-3">
+        <h2>회원가입</h2>
+      </div>
+      <Row className="justify-content-center">
+        <Col xs={12} sm={10} md={8} lg={6} xl={4}>
+          <Formik
+            initialValues={{ email: '', password: '', password2: '', username: ''}}
+            onSubmit={submit}
+            validationSchema={Yup.object().shape({
+              email: Yup.string()
+                .email("이메일형식으로 입력하세요")
+                .required("필수필드 입니다."),
+              password: Yup.string()
+                .required("Required")
+                .min(6, "6자이상 입력하세요"),
+              password2: Yup.string()
+                .oneOf([Yup.ref("password"), null], "패스워드가 일치하지 않습니다.")
+                .required("패스워드가 일치하지 않습니다."),
+              username: Yup.string()
+                .required("필수필드 입니다.")
+            })}>
+            {
+              ({values,
+                  errors,
+                  touched,
+                  handleChange,
+                  handleBlur,
+                  handleSubmit,
+                  isSubmitting }) => (<Form onSubmit={handleSubmit}>
+                <Form.Group controlId="email">
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control name="email" placeholder="Enter email"
+                                value={values.email}
+                                onChange={handleChange} onBlur={handleBlur}
+                                isValid={touched.email && !errors.email}
+                                isInvalid={touched.email && errors.email ? true : false} />
+                  { touched.email && !errors.email && <Form.Control.Feedback type="valid">Looks good!</Form.Control.Feedback> }
+                  { touched.email && errors.email && <Form.Control.Feedback type="invalid">{errors.email}</Form.Control.Feedback> }
+                </Form.Group>
+                <Form.Group controlId="formGroupPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control type="password" name="password" placeholder="enter Password"
+                                value={values.password}
+                                onChange={handleChange} onBlur={handleBlur}
+                                isValid={touched.password && !errors.password}
+                                isInvalid={touched.password && errors.password ? true : false} />
+                  { touched.password && !errors.password && <Form.Control.Feedback type="valid">Looks good!</Form.Control.Feedback> }
+                  { touched.password && errors.password && <Form.Control.Feedback type="invalid">{errors.password}</Form.Control.Feedback> }
+                </Form.Group>
+                <Form.Group controlId="formGroupPassword2">
+                  <Form.Label>confirm Password</Form.Label>
+                  <Form.Control type="password" name="password2" placeholder="confirm Password"
+                                value={values.password2}
+                                onChange={handleChange} onBlur={handleBlur}
+                                isValid={touched.password2 && !errors.password2}
+                                isInvalid={touched.password2 && errors.password2 ? true : false} />
+                  { touched.password2 && !errors.password2 && <Form.Control.Feedback type="valid">Looks good!</Form.Control.Feedback> }
+                  { touched.password2 && errors.password2 && <Form.Control.Feedback type="invalid">{errors.password2}</Form.Control.Feedback> }
+                </Form.Group>
+                <Form.Group controlId="formGroupName">
+                  <Form.Label>Name</Form.Label>
+                  <Form.Control type="text" name="username" placeholder="enter username"
+                                value={values.username}
+                                onChange={handleChange} onBlur={handleBlur}
+                                isValid={touched.username && !errors.username}
+                                isInvalid={touched.username && errors.username ? true : false} />
+                  { touched.username && !errors.username && <Form.Control.Feedback type="valid">Looks good!</Form.Control.Feedback> }
+                  { touched.username && errors.username && <Form.Control.Feedback type="invalid">{errors.username}</Form.Control.Feedback> }
+                </Form.Group>
+                <Button variant="primary" type="submit" disabled={isSubmitting}>
+                  Submit
+                </Button>
+              </Form>)
+            }
+          </Formik>
+        </Col>
+      </Row>
+    </>
   );
 }
 
